22114f3e719024b94e037e4aed890356a63ee672
dilomilo <dil3k@live.de>
2017-10-27 15:18:23 +0200

    aenderung

--- src/test/java/com/businessapp/customer/IndividualCustomerTest.java
+++ src/test/java/com/businessapp/customer/IndividualCustomerTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.businessapp.model;
+package com.businessapp.customer;
 
+import com.businessapp.model.IndividualCustomer;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
